8544cd624912dfc80c71dfed5b22eee0067e508d
dudakvo <dudakvo@gmail.com>
2021-08-30 00:27:53 +0300

    component cat edit

--- src/Components/Cat/Cat.js
+++ src/Components/Cat/Cat.js
@@ -4,13 +4,11 @@ export default function Cat(props) {
   const { img } = props;
   return (
     <div className={s.imgModule}>
-      {img.url && (
-        <img
-          src={`https://cataas.com${img.url}`}
-          alt={img.tags}
-          object-fit="contain"
-        />
-      )}
+      <img
+        src={`https://cataas.com${img.url}`}
+        alt={img.tags}
+        object-fit="contain"
+      />
     </div>
   );
 }
